d616ff54add1fd8a14a1b65f9319a0e3d0246575
scklepova <sckleppi@gmail.com>
2014-11-29 00:28:59 +0500

    Refactor. Extract methods

--- Calendar/MyCalendar.cs
+++ Calendar/MyCalendar.cs
@@ -40,110 +40,93 @@ namespace Calendar
         public void ShowCalendar()
         {
 
-            Bitmap bmp = new Bitmap(800, 800);
-            var width = 560;
+            const int imageSize = 800;
+            var bmp = new Bitmap(imageSize, imageSize);
+            const int widthCalendar = 560;
 
-            Graphics g = Graphics.FromImage(bmp);
+            var g = Graphics.FromImage(bmp);
+            g.FillRectangle(new SolidBrush(Color.FromArgb(8, 108, 179)), 0, 0, imageSize, imageSize);
 
-            g.SmoothingMode = SmoothingMode.AntiAlias;
-            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
-            g.PixelOffsetMode = PixelOffsetMode.HighQuality;
-            //g.DrawString("yourText", new Font("Tahoma", 32), Brushes.Black, rectf);
-
-            //g.Flush();
-            g.FillRectangle(new SolidBrush(Color.FromArgb(8, 108, 179)), 0, 0, 800, 800);
-
-            var stringFormat = new StringFormat();
-            stringFormat.Alignment = StringAlignment.Center;
-            stringFormat.LineAlignment = StringAlignment.Center;
-            
-            RectangleF thisDate = new RectangleF(120, 20, width, 140);
-            g.FillRectangle(new SolidBrush(Color.FromArgb(3, 201, 169)), thisDate);
-            g.DrawString(Date.Day.ToString(), new Font("Helvetica", 60), Brushes.White, thisDate, stringFormat);
-
-            var thisMonth = new RectangleF(120, 160, width, 80);
-            g.FillRectangle(Brushes.White, thisMonth);
-            g.DrawString(Date.ToString("MMMM", CultureInfo.GetCultureInfo("en-us")) + ", " + Date.Year.ToString(), new Font("Helvetica", 30), Brushes.Turquoise, thisMonth, stringFormat);
-
-
-            int col = 0;
-           
-            for (var dayOfWeek = DayOfWeek.Monday; dayOfWeek <= DayOfWeek.Saturday; dayOfWeek++)
+            var stringFormat = new StringFormat
             {
-                var thisDays = new RectangleF(120 + col * 80, 240, 80, 80);
-                g.FillRectangle(Brushes.WhiteSmoke, thisDays);
-                g.DrawString(dayOfWeek.ToString().Substring(0, 3), new Font("Helvetica", 20), Brushes.Turquoise, thisDays, stringFormat);
-                col++;
-            }
+                Alignment = StringAlignment.Center,
+                LineAlignment = StringAlignment.Center
+            };
+            CreateDate(widthCalendar, g, stringFormat);
+            CreateMonth(widthCalendar, g, stringFormat);
+            CreateDaysOfWeek(g, stringFormat);
+            CreateDays(g, stringFormat);
+            bmp.Save("calendar.png");
+        }
 
-            var sunday  = new RectangleF(120 + col * 80, 240, 80, 80);
-            g.FillRectangle(Brushes.WhiteSmoke, sunday);
-            g.DrawString("Sun", new Font("Helvetica", 20), Brushes.Turquoise, sunday, stringFormat);
-            //col++;
-
-
-            var row = 0;
-            col = 0;
+        private void CreateDays(Graphics g, StringFormat stringFormat)
+        {    
+            var col = 0;
             foreach (var dayOfWeek in Calendar.Keys)
             {
-                row = 0;
-                //var view = dayOfWeek.ToString().ToLower().Substring(0, 3);
+                var row = 0;
                 if (Calendar[dayOfWeek].First() > 1 &&
                     ((DayOfWeek) dayOfWeek < FirstDay().DayOfWeek && (DayOfWeek) dayOfWeek != DayOfWeek.Sunday ||
                      FirstDay().DayOfWeek == DayOfWeek.Sunday))
                 {
-                    //view += "     ";
-                    createDay(row, col, "", g, stringFormat, Brushes.WhiteSmoke);
+                    CreateDay(row, col, "", g, stringFormat, Brushes.WhiteSmoke);
                     row++;
-
                 }
-                    
 
-                
+
                 foreach (var day in Calendar[dayOfWeek])
                 {
-                    var viewDay = day.ToString();
-                    if (viewDay.Length == 1)
-                        viewDay = " " + viewDay;
-
-                    if (day == Date.Day)
-                        viewDay = " [" + viewDay + "]";
-                    else
-                        viewDay = "  " + viewDay + " ";
-
-              
-                    createDay(row, col, day.ToString(), g, stringFormat, day == Date.Day ? new SolidBrush(Color.FromArgb(225, 225, 225)) : Brushes.WhiteSmoke);
-                    //view += viewDay;
+                    CreateDay(row, col, day.ToString(), g, stringFormat,
+                        day == Date.Day ? new SolidBrush(Color.FromArgb(225, 225, 225)) : Brushes.WhiteSmoke);
                     row++;
-
                 }
 
-                
-
-//                if (view.EndsWith(" "))
-//                    view = view.Substring(0, view.Length - 1);
-
-                //Console.WriteLine(view);
-                //g.DrawString("yourText", new Font("Tahoma", 32), Brushes.Black, rectf);
-
-                
 
                 if (LastDay().DayOfWeek != DayOfWeek.Sunday && Calendar[dayOfWeek].Last() < LastDay().Day &&
                     ((DayOfWeek) dayOfWeek > LastDay().DayOfWeek || (DayOfWeek) dayOfWeek == DayOfWeek.Sunday))
                 {
-                    createDay(row, col, "", g, stringFormat, Brushes.WhiteSmoke);
+                    CreateDay(row, col, "", g, stringFormat, Brushes.WhiteSmoke);
                 }
-                    //view += "     ";
-
                 col++;
             }
-
-            Console.WriteLine(LastDay().DayOfWeek);
-
-            bmp.Save("calendar.png");
         }
 
-        static void createDay(int row, int col, string text, Graphics g, StringFormat stringFormat, Brush background)
+        private static void CreateDaysOfWeek(Graphics g, StringFormat stringFormat)
+        {
+            var col = 0;
+            for (var dayOfWeek = DayOfWeek.Monday; dayOfWeek <= DayOfWeek.Saturday; dayOfWeek++)
+            {
+                CreateDayOfWeek(g, stringFormat, col, dayOfWeek);
+                col++;
+            }
+            CreateDayOfWeek(g, stringFormat, col, DayOfWeek.Sunday);
+        }
+
+
+        private static void CreateDayOfWeek(Graphics g, StringFormat stringFormat, int col, DayOfWeek dayOfWeek)
+        {
+            var thisDays = new RectangleF(120 + col*80, 240, 80, 80);
+            g.FillRectangle(Brushes.WhiteSmoke, thisDays);
+            g.DrawString(dayOfWeek.ToString().Substring(0, 3), new Font("Helvetica", 20), Brushes.Turquoise, thisDays,
+                stringFormat);
+        }
+
+        private static void CreateMonth(int widthCalendar, Graphics g, StringFormat stringFormat)
+        {
+            var thisMonth = new RectangleF(120, 160, widthCalendar, 80);
+            g.FillRectangle(Brushes.White, thisMonth);
+            g.DrawString(Date.ToString("MMMM", CultureInfo.GetCultureInfo("en-us")) + ", " + Date.Year.ToString(),
+                new Font("Helvetica", 30), Brushes.Turquoise, thisMonth, stringFormat);
+        }
+
+        private static void CreateDate(int widthCalendar, Graphics g, StringFormat stringFormat)
+        {
+            var thisDate = new RectangleF(120, 20, widthCalendar, 140);
+            g.FillRectangle(new SolidBrush(Color.FromArgb(3, 201, 169)), thisDate);
+            g.DrawString(Date.Day.ToString(), new Font("Helvetica", 60), Brushes.White, thisDate, stringFormat);
+        }
+
+        static void CreateDay(int row, int col, string text, Graphics g, StringFormat stringFormat, Brush background)
         {
             var day = new RectangleF(120 + col * 80, 320 + row * 80, 80, 80);
             g.FillRectangle(background, day);
